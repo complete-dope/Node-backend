3ed2333c89b95e65fd58fe03cee10faef05fbba8
complete-dope <mdulani2002@gmail.com>
2022-02-12 13:50:58 +0530

    Part-2 (Sync vs async)

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,5 @@ setTimeout(()=>{
 },0)
 
 console.log("stopping");
+// This is about the call stack and callback queue 
+// call stack takes the js functions in it and the functions like settimeout is a node api function and it goes in the callback queue whereas normal functions goes in the call stack and callback queue functions will always be called after the call stack function 
