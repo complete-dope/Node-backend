a1344cee7d3a30e766959b5844681055be702193
complete-dope <mdulani2002@gmail.com>
2022-02-12 14:43:00 +0530

    part-4

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,14 @@
 const request = require('request');
 // request version 2.88.0
 
-const url = "https://api.weatherapi.com/v1/current.json?key=4af4dda2afeb4dfab5d81100221202&q=London&aqi=yes"
+const url = "https://api.weatherapi.com/v1/current.json?key=4af4dda2afeb4dfab5d81100221202&q=Kerala&aqi=yes"
 
-request({url:url} , (error , response)=>{
-    const data = JSON.parse(response.body);
+request({url:url , json:true} , (error , response)=>{
+    // json=true will automatically parse the json into object 
+    // const data = JSON.parse(response.body);
+    // console.log("The current temp is " +data.current.temp_c);
+    // console.log("The weather their is "+data.current.condition.text);
+    const data = response.body;
     console.log("The current temp is " +data.current.temp_c);
     console.log("The weather their is "+data.current.condition.text);
 })
