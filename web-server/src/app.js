3ca3f576c7152d5b88040fcaec1418b4043b21b5
complete-dope <mdulani2002@gmail.com>
2022-02-13 08:45:20 +0530

    Part-8

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,28 @@
 const path = require('path'); // Path Library of NodejS
 const express= require('express');//express is a function
 const res = require('express/lib/response');
-console.log(__dirname);
-console.log(__filename);
-console.log(path.join(__dirname  ,"../public/index.html"));
-
+const hbs = require("hbs")
 // we call it to create a new express server
 const app =express();
+const viewsPath = path.join(__dirname  ,"../templates" )
+const publicDirectoryPath = path.join(__dirname , '../public')
+
+// app.set() 
+app.set('view engine','hbs')
+app.set('views' , viewsPath)
 
 // app.use() customises server
-app.use(express.static(path.join(__dirname  ,"../public")))
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+// Routes
 
 app.get('' ,(req,res)=>{
     // res.send("Hello")
-    res.send({"name":"Maanu"})
+    // res.send({"name":"Maanu"})
+    res.render('index',{
+        title:'Weather',
+        name:"Elon"
+    })
 })
 // app.com -->express server
 // app.com/help --> routes
