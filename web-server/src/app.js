f39968b472fcb1fab060b66e577529b343da738c
complete-dope <mdulani2002@gmail.com>
2022-02-13 17:06:40 +0530

    Part-10(get location by query url)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,22 +2,40 @@ const path = require('path'); // Path Library of NodejS
 const express= require('express');//express is a function
 const res = require('express/lib/response');
 const hbs = require("hbs")
+const forecastList = require("./utils/forecast.js");
+const { log } = require('console');
 // we call it to create a new express server
+
 const app =express();
 const viewsPath = path.join(__dirname  ,"../templates/views" )
 const publicDirectoryPath = path.join(__dirname , '../public')
 const partialsPath = path.join(__dirname , '../templates/partials')
+
+const request = require("request")
+
+function apilocation(location){
+    const url = "https://api.weatherapi.com/v1/current.json?key=4af4dda2afeb4dfab5d81100221202&q=" + encodeURIComponent(location) + "&aqi=yes";
+    request({url:url , json:true} , (error , response)=>{
+        console.log(response.body.current.temp_c);
+        var b = response.body.current.temp_c
+        console.log(b);
+        return b;
+    })
+}
+
+
+
 // app.set() 
 app.set('view engine','hbs')
 app.set('views' , viewsPath)
 hbs.registerPartials(partialsPath)
+
 // app.use() customises server
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-// Routes
 
+// Routes
 app.get('/' ,(req,res)=>{
-    // res.send("Hello")
     res.render('index',{
         title:'Weather',
         name:"Elon"
@@ -25,16 +43,31 @@ app.get('/' ,(req,res)=>{
 })
 
 app.get('/weather' ,(req,res)=>{
-    // res.send("Hello")
-    // res.send({"name":"Elon"})
     if(!req.query.address){
         return res.send({error:"Please enter a search query"})
     }
-    res.render('weather',{
-        forecast:'Sunny',
-        location:"Gurgaon",
-        address:req.query.address
+    const location =req.query.address;
+    const url = "https://api.weatherapi.com/v1/current.json?key=4af4dda2afeb4dfab5d81100221202&q=" + encodeURIComponent(location) + "&aqi=yes";
+    request({url:url , json:true} , (error , response)=>{
+        if(error){
+            return res.send("this file cant be sent")
+        }
+        var text = response.body.current.condition.text // this gives MIST
+        var humidity =response.body.current.humidity // this gives 32
+        var feelslike_c = response.body.current.feelslike_c // feels like temp
+        var aqi =response.body.current.air_quality.pm2_5 // this gives 107.45
+
+        var temp_c = response.body.current.temp_c
+        res.render('weather',{
+            temp_c:temp_c,
+            location:req.query.address,
+            condition:text,
+            humidity:humidity,
+            feelslike_c:feelslike_c,
+            aqi:aqi
+        })
     })
+    
 })
 // app.com -->express server
 // app.com/help --> routes
