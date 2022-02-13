cedc255b3be3751c881635233d21c188e510e81d
complete-dope <mdulani2002@gmail.com>
2022-02-13 08:06:33 +0530

    Part-8 (express and its libraries)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,29 @@
+const path = require('path'); // Path Library of NodejS
 const express= require('express');//express is a function
+const res = require('express/lib/response');
+console.log(__dirname);
+console.log(__filename);
+console.log(path.join(__dirname  ,"../public/index.html"));
 
 // we call it to create a new express server
 const app =express();
 
+// app.use() customises server
+app.use(express.static(path.join(__dirname  ,"../public")))
+
 app.get('' ,(req,res)=>{
-    res.send("Hello")
+    // res.send("Hello")
+    res.send({"name":"Maanu"})
 })
 // app.com -->express server
 // app.com/help --> routes
 
-app.get('/help' , ()=>{
-    console.log("This is Help function");
+app.get('/help' , (req,res)=>{
+    res.send("This is Help function");
+})
+
+app.get('/about' ,(req,res)=>{
+    res.send('<h1>Hello About</h1>')
 })
 
 app.listen(5000 ,()=>{
