ac382e8327cadce7a3622a2e3cda6a1afa13920e
complete-dope <mdulani2002@gmail.com>
2022-02-13 09:41:15 +0530

    part-9(handlerbar)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,13 +4,13 @@ const res = require('express/lib/response');
 const hbs = require("hbs")
 // we call it to create a new express server
 const app =express();
-const viewsPath = path.join(__dirname  ,"../templates" )
+const viewsPath = path.join(__dirname  ,"../templates/views" )
 const publicDirectoryPath = path.join(__dirname , '../public')
-
+const partialsPath = path.join(__dirname , '../templates/partials')
 // app.set() 
 app.set('view engine','hbs')
 app.set('views' , viewsPath)
-
+hbs.registerPartials(partialsPath)
 // app.use() customises server
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -32,7 +32,11 @@ app.get('/help' , (req,res)=>{
 })
 
 app.get('/about' ,(req,res)=>{
-    res.send('<h1>Hello About</h1>')
+    res.render('about',{
+        title:"About",
+        name:"Elon"
+    })
+    // here title and name are the props that we pass to the (similar to the one we used in the react application)
 })
 
 app.listen(5000 ,()=>{
