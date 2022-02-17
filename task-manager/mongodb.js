088c98d3c8a814c5dfc76a1528c2890080844246
complete-dope <mdulani2002@gmail.com>
2022-02-17 13:44:26 +0530

    Part-4

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,4 +1,9 @@
 // CRUD operations
+
+// for creating data we use insert and insertOne
+// for reading data we use find and findOne
+// for updating data we use update and updateOne
+// for deleting we use delete 
 const mongodb = require('mongodb') // mongodb version 3.1.10
 const MongoClient =  mongodb.MongoClient
 const connectionURL =  'mongodb://127.0.0.1:27017' //this is the default url for connection string
@@ -28,22 +33,22 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(error , client)=>{
     //     console.log(result);
     // })
     //this creates a database named task-manager with a collection called users and it will have this data in it .
-    db.collection('users').insertMany([
-        {
-            name:"Joe",
-            age:35
-        },
-        {
-            name:"Monica",
-            age:28
-        }
-    ],(error , result)=>{
-        if(error){
-            return console.log("unable to insert user");
-        }
-        console.log("i am here");
-        console.log(result);
-    })
+    // db.collection('users').insertMany([
+    //     {
+    //         name:"Joe",
+    //         age:35
+    //     },
+    //     {
+    //         name:"Monica",
+    //         age:28
+    //     }
+    // ],(error , result)=>{
+    //     if(error){
+    //         return console.log("unable to insert user");
+    //     }
+    //     console.log("i am here");
+    //     console.log(result);
+    // })
 
     //finding the data of a person by passing its name
     db.collection('users').findOne({name:"Joe"} , (error , user)=>{
@@ -57,5 +62,39 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(error , client)=>{
         console.log(users);
     })
     //returns all the values with age = 52 and returns the array of them
+
+    // for update we need to pass the $set object
+    // https://docs.mongodb.com/manual/reference/operator/update/
+    db.collection('users').insertMany([
+        {
+            name:"raman",
+            age:12
+        },
+        {
+            name:"rohit",
+            age:12
+        }
+    ])
+    // update one and update many
+    db.collection('users').updateOne(
+        { _id : new ObjectID("620d12e4d0c0a51b503d6a0d") },{
+        $set:{
+            name:"tribiani"
+        } ,
+        $inc:{
+            age:1
+        }
+    })
+
+    db.collection('users').updateMany({age:12},{
+        $inc:{
+            age:1
+        }
+    })
+
+    //delete one and delete many
+    db.collection('users').deleteMany({age:14})
+    db.collection('users').deleteOne({name:'Elon'})
 })
 
+ 
