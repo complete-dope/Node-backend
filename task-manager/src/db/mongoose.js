9c3c721a11a8c52535e5dc0bf80c6b7b4db5a78b
complete-dope <mdulani2002@gmail.com>
2022-02-18 17:19:42 +0530

    Validation-1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
+const validator = require('validator')
+// Rather than making custom validators here we are usign the library provided by npm called validator js
+
 console.log("Connected ");
 
 //mongoose is just a add on to mongodb as in it helps to perform more than just basic CRUD operations .
@@ -10,16 +13,36 @@ console.log("Connected ");
 const user = mongoose.model('Users',{
     name:{
         type:String,
-
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            //value is email here
+            if(!validator.isEmail(value)){
+                throw new Error("This is not a Email")
+            }
+        }
     },
     age:{
-        type:Number // If passed a string this should be stored as Number in the database
+        type:Number, // If passed a string this should be stored as Number in the database
+        //here we are providing custom validation
+        default:0,
+        validate(value){
+            if(value<0){
+                throw new Error("Age should be positive")
+            }
+        }
     }
 })
 
 const me = new user({
-    name:"Elon",
-    age:53
+    name:"  Elon  ",
+    email:"helo@gmail.com  "
 })
 
 me.save().then(() => {
