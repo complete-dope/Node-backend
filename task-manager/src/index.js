29fa8e29ff40ff788aa7e27f626db082c6079530
complete-dope <mdulani2002@gmail.com>
2022-02-22 16:27:54 +0530

    JWT intro

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,7 +17,17 @@ app.use(userRouter)
 app.listen(port,()=>{
     console.log("the App has started at port " , port);
 })
-
+// this is token for login the user and earlier one was just a 
+const jwt = require('jsonwebtoken')
+const myfunction =async ()=>{
+    const token = jwt.sign({ _id:'abc123' },'this is a new thing', {expiresIn:'2 seconds'})
+    console.log(token);
+    const data = jwt.verify(token , 'this is a new thing')
+    console.log(data);
+}
+myfunction()
 
 //hashed method in bcrypt passes a promise as a return function
 //whenever a single file gets larger and larger their is  always a potential to break in down in smaller parts by routing them or creating new files and have smaller code in it
+// jWT is made up of 3 parts header , payload(data we provided) , signature(the string which we have provided up their act as a secret )
+// wrkflow is use jwt token to create a token for users and then use jwt verify to verify the user 
